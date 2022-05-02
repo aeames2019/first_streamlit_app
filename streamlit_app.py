4dc2725687a0e89e4d25ed536e3c545631fdb189
aeames2019 <46443355+aeames2019@users.noreply.github.com>
2022-05-02 23:02:51 +1000

    Update streamlit_app.py
    updated

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,14 +15,14 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
-#let's put a pick list here so they can pick the fruits they want to include 
+# let's put a pick list here so they can pick the fruits they want to include 
 fruits_selected = streamlit.multiselect("pick some fruits:", list(my_fruit_list.index),['Avocado', 'Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # display the table on a page
 streamlit.dataframe(fruits_to_show)
 
-fruitvice_reponse = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruitvice_reponse = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 streamlit.text(fruitvice_reponse.json())
 
 # take the json format and normalize it
